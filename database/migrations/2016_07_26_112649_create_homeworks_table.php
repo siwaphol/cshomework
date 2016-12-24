3346ff687e3f880d195d01a5b71e208fca20c8ec
Nong <nong-siwa@hotmail.com>
2016-12-24 14:20:51 +0700

    Add entrust library migration file

--- database/migrations/2016_07_26_112649_create_homeworks_table.php
+++ database/migrations/2016_07_26_112649_create_homeworks_table.php
@@ -27,8 +27,9 @@ class CreateHomeworksTable extends Migration
 
             $table->integer('teacher_id')->unsigned();
             $table->foreign('teacher_id')->references('id')->on('users');
-            $table->integer('semester_id')->unsigned();
-            $table->foreign('semester_id')->references('id')->on('semesters');
+            // semester ได้มาจากตาราง sections แล้วไม่จำเป็นต้องจัดเก็บซ้ำ
+//            $table->integer('semester_id')->unsigned();
+//            $table->foreign('semester_id')->references('id')->on('semesters');
 
             $table->integer('homework_type_id')->unsigned();
             $table->foreign('homework_type_id')->references('id')->on('homework_types');
