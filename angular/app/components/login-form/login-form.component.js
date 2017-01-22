20e8678e8042bc02534160d033033c8bcbf6fecb
Nong <nong-siwa@hotmail.com>
2017-01-22 19:09:34 +0700

    Link  from login page to after-login page

--- angular/app/components/login-form/login-form.component.js
+++ angular/app/components/login-form/login-form.component.js
@@ -1,9 +1,10 @@
 class LoginFormController {
-	constructor($auth, ToastService) {
+	constructor($auth, ToastService, $state) {
 		'ngInject';
 
 		this.$auth = $auth;
 		this.ToastService = ToastService;
+		this.$state = $state;
 	}
 
     $onInit(){
@@ -22,6 +23,7 @@ class LoginFormController {
 				this.$auth.setToken(response.data);
 
 				this.ToastService.show('Logged in successfully.');
+                this.$state.go('app.after-login');
 			})
 			.catch(this.failedLogin.bind(this));
 	}
